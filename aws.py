a38611995342d7c833b1c72ec965ff34acc4bf10
btowns <bill.townsley@nike.com>
2015-08-27 14:17:40 -0700

    confirmed termination works

--- aws.py
+++ aws.py
@@ -7,17 +7,23 @@ reservation = ec2.run_instances(image_id='ami-29ebb519',
                                 key_name='devenv-key',
                                 instance_type='t2.micro',
                                 security_group_ids=['devenv-sg'])
-print("Reservation ID = ", reservation.id)
+print "Reservation ID = ", reservation.id
 instance = reservation.instances[0]
+print "Instance ID is", instance.id
+
 while instance.state != "running":
     print "Instance state is", instance.state, "..."
     time.sleep(5)
     instance.update()
 print "Instance state is", instance.state, "!"
-print "Instance ID is", instance.id
+
 print instance.public_dns_name
 
-time.sleep(60)
-
 ec2.terminate_instances(instance.id)
+while instance.state != "terminated":
+    print "Instance state is", instance.state, "..."
+    time.sleep(5)
+    instance.update()
+print "Instance state is", instance.state, "!"
+
 
